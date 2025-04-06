dbedc6f5507e4cca5e8deb67ca5225de53cfd9d8
Evandro Sacramento II <evandro-dev@outlook.com>
2025-04-06 16:52:36 -0300

    mudança da classe Consulta

--- src/main/java/com/sistema/clinica/models/Consulta.java
+++ src/main/java/com/sistema/clinica/models/Consulta.java
@@ -3,6 +3,9 @@ package com.sistema.clinica.models;
 import com.sistema.clinica.models.enums.MetodoPagamento;
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @Entity
 public class Consulta {
 
@@ -10,14 +13,21 @@ public class Consulta {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private LocalDate data;
+    private LocalTime hora;
+    private boolean pagamentoRealizado;
+
     @Enumerated(EnumType.STRING)
     private MetodoPagamento metodoPagamento;
 
     public Consulta() {
     }
 
-    public Consulta(Long id, MetodoPagamento metodoPagamento) {
+    public Consulta(Long id, LocalDate data, LocalTime hora, boolean pagamentoRealizado, MetodoPagamento metodoPagamento) {
         this.id = id;
+        this.data = data;
+        this.hora = hora;
+        this.pagamentoRealizado = pagamentoRealizado;
         this.metodoPagamento = metodoPagamento;
     }
 
@@ -27,6 +37,30 @@ public class Consulta {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public boolean isPagamentoRealizado() {
+        return pagamentoRealizado;
+    }
+
+    public void setPagamentoRealizado(boolean pagamentoRealizado) {
+        this.pagamentoRealizado = pagamentoRealizado;
     }
 
     public MetodoPagamento getMetodoPagamento() {
