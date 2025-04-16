33e3362b11c9ac18440ed1d007b71d742376f256
Evandro Sacramento <evandro-dev@outlook.com>
2025-04-16 13:54:07 -0300

    cinserto na lista de consultas em admin/dashboard

--- src/main/java/com/sistema/clinica/controllers/page/AdminPageController.java
+++ src/main/java/com/sistema/clinica/controllers/page/AdminPageController.java
@@ -16,6 +16,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Controller
@@ -44,15 +45,27 @@ public class AdminPageController {
         Pessoa pessoa = pessoaRepository.findByUsernameIgnoreCase(pessoaDetails.getUsername())
                 .orElseThrow(() -> new RuntimeException("Pessoa não encontrada"));
 
-        // Aqui fazemos cast seguro, já que só pacientes acessam esse endpoint
+        // Aqui fazemos cast seguro, já que só admin acessam esse endpoint
         Funcionario funcionario = (Funcionario) pessoa;
 
         model.addAttribute("titulo", "Dashboard");
         model.addAttribute("pessoa", pessoa);
         model.addAttribute("conteudo", "admin/dashboard");
 
-        List<Consulta> proximasConsultas = agendaService.proximasConsultas();
-        model.addAttribute("consultas", proximasConsultas);
+
+        List<String> descricoesConsultas = agendaService.proximasConsultas().stream()
+                .map(c -> {
+                    String data = c.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                    String hora = c.getHora().format(DateTimeFormatter.ofPattern("HH:mm"));
+                    return data + " - " + hora + " - " + c.getPaciente().getNome()
+                            + " com Dr(a). " + c.getMedico().getNome()
+                            + " (" + c.getMedico().getEspecialidade() + ")";
+                }).toList();
+
+        model.addAttribute("descricoesConsultas", descricoesConsultas);
+
+
+
         List<Medico> medicos = medicoRepository.findAll();
         List<Funcionario> funcionarios = funcionarioRepository.findAll();
         model.addAttribute("medicos", medicos);
@@ -61,6 +74,8 @@ public class AdminPageController {
 
         return "layout";
     }
+
+
 
 
     @GetMapping("/")
