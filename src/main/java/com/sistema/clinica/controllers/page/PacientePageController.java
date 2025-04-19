8a8320d00b9791cccb00ce0e7c298bcaa37f9ee7
Evandro Sacramento II <evandro-dev@outlook.com>
2025-04-19 18:09:19 -0300

    Criaçã e autorização da rota do paciente paciente/agendamento-consulta

--- src/main/java/com/sistema/clinica/controllers/page/PacientePageController.java
+++ src/main/java/com/sistema/clinica/controllers/page/PacientePageController.java
@@ -1,10 +1,12 @@
 package com.sistema.clinica.controllers.page;
 
 import com.sistema.clinica.models.*;
+import com.sistema.clinica.repositories.MedicoRepository;
 import com.sistema.clinica.repositories.PacienteRepository;
 import com.sistema.clinica.repositories.PessoaRepository;
 import com.sistema.clinica.security.PessoaDetails;
 import com.sistema.clinica.services.AgendaService;
+import com.sistema.clinica.services.ConsultaService;
 import com.sistema.clinica.services.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.sql.RowSet;
 import java.util.List;
 
 @Controller
@@ -30,13 +33,26 @@ public class PacientePageController {
 
     @Autowired
     private AgendaService agendaService;
+    private final ConsultaService consultaService;
 
+    private final MedicoRepository medicoRepository;
+    private final PacienteRepository pacienteRepository;
+
+    public PacientePageController(ConsultaService consultaService, MedicoRepository medicoRepository, PacienteRepository pacienteRepository) {
+        this.consultaService = consultaService;
+        this.medicoRepository = medicoRepository;
+        this.pacienteRepository = pacienteRepository;
+    }
+
+
+    //Metodo sem o thymeleaf
     @GetMapping("/cadastro")
     public String mostrarFormularioCadastro(Model model) {
 
         return "paciente/cadastroPaciente";
     }
 
+    //Metodo sem o thymeleaf
 
     @PostMapping
     public String salvarViaFormulario(@ModelAttribute Paciente paciente, RedirectAttributes redirectAttributes) {
@@ -50,7 +66,7 @@ public class PacientePageController {
 
 
     @GetMapping("/minha-area")
-    public String abrirCadastroConsulta(Model model, @AuthenticationPrincipal PessoaDetails pessoaDetails) {
+    public String abrirPacienteArea(Model model, @AuthenticationPrincipal PessoaDetails pessoaDetails) {
         Pessoa pessoa = pessoaRepository.findByUsernameIgnoreCase(pessoaDetails.getUsername())
                 .orElseThrow(() -> new RuntimeException("Pessoa não encontrada"));
 
@@ -62,5 +78,60 @@ public class PacientePageController {
         model.addAttribute("conteudo", "paciente/minhaArea");
 
         return "layout";
+    }
+
+    @GetMapping("/agendamento-consulta")
+    public String abrirCadastroConsulta(Model model, @AuthenticationPrincipal PessoaDetails pessoaDetails) {
+        Pessoa pessoa = pessoaRepository.findByUsernameIgnoreCase(pessoaDetails.getUsername())
+                .orElseThrow(() -> new RuntimeException("Pessoa não encontrada"));
+
+        // Aqui fazemos cast seguro, já que só pacienete acessam esse endpoint
+        Paciente paciente = (Paciente) pessoa;
+
+        model.addAttribute("titulo", "Agendamento de Consultas");
+        model.addAttribute("pessoa", pessoa);
+        model.addAttribute("conteudo", "paciente/cadastroConsulta");
+
+        model.addAttribute("pacientes", pacienteRepository.findAll());
+        model.addAttribute("especialidades", medicoRepository.findEspecialidades());
+
+
+
+        return "layout";
+
+    }
+
+
+    @PostMapping("/agendamento-consulta")
+    public String salvarAgendametoConsultas(@ModelAttribute Consulta consulta,
+                                            RedirectAttributes redirectAttributes,
+                                            Model model,
+                                            @AuthenticationPrincipal PessoaDetails pessoaDetails) {
+        try {
+            System.out.println("Dados recebidos: " + consulta);
+            Long idMedico = consulta.getMedico().getId();
+            Medico medico = medicoRepository.findById(idMedico)
+                    .orElseThrow(() -> new RuntimeException("Médico não encontrado"));
+
+
+
+            Pessoa pessoa = pessoaRepository.findByUsernameIgnoreCase(pessoaDetails.getUsername())
+                    .orElseThrow(() -> new RuntimeException("Pessoa não encontrada"));
+
+
+            Paciente paciente = (Paciente) pessoa; // cast seguro
+
+            consulta.setMedico(medico);
+            consulta.setPaciente(paciente);
+            consultaService.insert(consulta);
+            redirectAttributes.addFlashAttribute("mensagem",
+                    "Consulta com o médico(a) Dr(a) \"" + consulta.getMedico().getNome() + "\" agendada com sucesso!");
+        } catch (Exception e) {
+            System.err.println("Erro ao cadastrar consulta: " + e.getMessage());
+            e.printStackTrace();
+            redirectAttributes.addFlashAttribute("erro",
+                    "Erro ao cadastrar consulta: " + e.getMessage());
+        }
+        return "redirect:/paciente/agendamento-consulta";
     }
 }
